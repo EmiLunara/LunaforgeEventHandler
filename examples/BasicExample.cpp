73cc71fce665cc51b78ba6dd490f17600cade32d
Emily <emily.carlsson92@gmail.com>
2026-08-21 00:56:15 +0200

    cleanup

--- examples/BasicExample.cpp
+++ examples/BasicExample.cpp
@@ -1,21 +1,39 @@
 #include <EventHandler/EventBus.h>
+
 #include <iostream>
+#include <string>
 
 struct PlayerDamaged
 {
     int Damage;
 };
 
+struct ItemPickedUp
+{
+    std::string ItemName;
+};
+
 int main()
 {
     Lunaforge::Events::EventBus Bus;
 
-    auto UI = Bus.Subscribe<PlayerDamaged>([](const PlayerDamaged& Event)
-        {
-            printf("Player took %d damage.\n", Event.Damage);
-        });
+    auto UISubscription = Bus.Subscribe<PlayerDamaged>([](const PlayerDamaged& Event)
+    {
+        printf("[UI] Player took %d damage.\n", Event.Damage);
+    });
+    
+    auto AudioSubscription = Bus.Subscribe<PlayerDamaged>([](const PlayerDamaged&)
+    {
+        printf("[Audio] Play damage sound.\n");
+    });
+
+    auto InventorySubscription = Bus.Subscribe<ItemPickedUp>([](const ItemPickedUp& Event)
+    {
+        printf("[Inventory] Picked up %s.\n", Event.ItemName.c_str());
+    });
     
     Bus.Publish(PlayerDamaged{25});
-    
+    Bus.Publish(ItemPickedUp{"Health Potion"});
+
     return 0;
 }
